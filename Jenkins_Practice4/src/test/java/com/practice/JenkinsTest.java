e3053b193e71e82bc7dc34442d24a6d30e5847e8
laksh <laksh@LakshmananSelva>
2025-08-01 12:11:45 +0530

    commit message

---
+++ Jenkins_Practice4/src/test/java/com/practice/JenkinsTest.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest 
+{
+	@Test
+	public void practiceTest()
+	{
+		System.out.println("Jenkins are executed");
+	}
+
+}
---
+++ Jenkins_Practice4/src/test/java/com/sample/Project.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+public class Project 
+{
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("Projects are Executed");
+	}
+
+}
